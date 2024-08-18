8b79e294e03de0d738d5559d2818353229336ead
AMINE LEBRINI <113290995+aminelebrini@users.noreply.github.com>
2024-08-18 18:49:56 +0100

    String Manipulation Module

---
+++ String-Manipulation-Module/stringUtils.js
@@ -0,0 +1,10 @@
+function capitalize(str){
+    if(str.length=== 0){
+        return str;
+    }
+        return str.charAt(0).toUpperCase()+str.slice(1);
+    
+    
+}
+
+export default capitalize;
